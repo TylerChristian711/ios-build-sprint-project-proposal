7a71068fef12eb67c268ea162507869fb329ab7d
Tyler Christian <tylerc71197@gmail.com>
2020-03-03 17:43:55 -0500

    Added a date formatter as to track what date the item was created going to also change the date to .medium as to get a more accurate date for when a new entry was created

---
+++ CountabilityTracker/CountabilityTracker/DateTimeFormatter.swift
@@ -0,0 +1,23 @@
+//
+//  DateTimeFormatter.swift
+//  CountabilityTracker
+//
+//  Created by Lambda_School_Loaner_218 on 3/3/20.
+//  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import Foundation
+
+class DateTimeFormatter {
+    
+    static func formatDateStamp(for entry: Entry) -> String {
+        return dateFormatter.string(from: entry.date!)
+    }
+    
+    static let dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.timeStyle = .none
+        formatter.dateStyle = .short
+        return formatter
+    }()
+}
