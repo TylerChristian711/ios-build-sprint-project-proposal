f89989c7d761e55a5f49aa7e9cc43dc3725168ee
Tyler Christian <tylerc71197@gmail.com>
2020-03-05 10:43:34 -0500

    Notifications s till need to be worked on as they are not being sent

--- CountabilityTracker/CountabilityTracker/ViewControllers/EntryTableViewController.swift
+++ CountabilityTracker/CountabilityTracker/ViewControllers/EntryTableViewController.swift
@@ -12,6 +12,7 @@ import CoreData
 class EntryTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
     
     let entryController = EntryController()
+//    let settings = SettingsViewController()
     
     
     lazy var fetchedResultsController: NSFetchedResultsController<Entry> = {
@@ -39,6 +40,38 @@ class EntryTableViewController: UITableViewController, NSFetchedResultsControlle
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refresh(_:)), for: .valueChanged)
         self.refreshControl = refreshControl
+//        SettingsViewController.notification(SettingsViewController)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "CountAbilityTracker"
+        content.body = "Time to reflect on the day!"
+        content.sound = .default
+        
+        let gregorian = Calendar(identifier: .gregorian)
+        let now = Date()
+        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+        
+        components.hour = 7
+        components.minute = 55
+        components.second = 0
+        
+        let date = gregorian.date(from: components)!
+        
+        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second], from: date)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
+        
+        let identifier = "UYLLocalNotification"
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        
+        
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Something is wrong most liekly on line 40 and 39: \(error.localizedDescription)")
+                     print("The time is 7:45")
+            }
+            
+        }
+        
         
         refresh(nil)
     }
--- CountabilityTracker/CountabilityTracker/ViewControllers/SettingsViewController.swift
+++ CountabilityTracker/CountabilityTracker/ViewControllers/SettingsViewController.swift
@@ -5,48 +5,48 @@
 //  Created by Lambda_School_Loaner_218 on 3/3/20.
 //  Copyright © 2020 Lambda_School_Loaner_218. All rights reserved.
 //
-
-import UIKit
-import UserNotifications
-
-class SettingsViewController: UIViewController {
-    
-    @IBOutlet weak var timeReminder: UIDatePicker!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    func notification() {
-        let content = UNMutableNotificationContent()
-        content.title = "CountAbilityTracker"
-        content.body = "Time to reflect on the day!"
-        content.sound = .default
-        
-        let gregorian = Calendar(identifier: .gregorian)
-        let now = Date()
-        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
-        
-        components.hour = 7
-        components.minute = 30
-        components.second = 0
-        
-        let date = gregorian.date(from: components)!
-        
-        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second], from: date)
-        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
-        
-        let identifier = "UYLLocalNotification"
-        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-        
-        
-        UNUserNotificationCenter.current().add(request) { (error) in
-            if let error = error {
-                print("Something is wrong most liekly on line 40 and 39: \(error.localizedDescription)")
-            }
-        }
-    }
-    
-    
-
-}
+//
+//import UIKit
+//import UserNotifications
+//
+//class SettingsViewController: UIViewController {
+//    
+//    @IBOutlet weak var timeReminder: UIDatePicker!
+//    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//    }
+//
+////    func notification() {
+////        let content = UNMutableNotificationContent()
+////        content.title = "CountAbilityTracker"
+////        content.body = "Time to reflect on the day!"
+////        content.sound = .default
+////
+////        let gregorian = Calendar(identifier: .gregorian)
+////        let now = Date()
+////        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
+////
+////        components.hour = 7
+////        components.minute = 30
+////        components.second = 0
+////
+////        let date = gregorian.date(from: components)!
+////
+////        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second], from: date)
+////        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
+////
+////        let identifier = "UYLLocalNotification"
+////        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+////
+////
+////        UNUserNotificationCenter.current().add(request) { (error) in
+////            if let error = error {
+////                print("Something is wrong most liekly on line 40 and 39: \(error.localizedDescription)")
+////            }
+////        }
+//    }
+//    
+//    
+//
+//}
